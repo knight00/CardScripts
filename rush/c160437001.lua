3fea8dcedac15339c39d8b2eaaecb79408fd892c
knight00 <perfectdicky@yahoo.com.hk>
2023-01-12 17:42:04 +0800

    auto zip by AI

--- rush/c160437001.lua
+++
@@ -1,4 +0,0 @@
---万能地雷グレイモヤ
---Widespread Ruin (Rush)
---Alt artwork
-Duel.LoadScript("c160436005.lua")
--- unofficial/c511027670.lua
+++
@@ -1,8 +0,0 @@
---ミラーイマジン・リフレクター１ワン-1
---Mirror Imagine Reflector 1
---Made by Beetron-1 Beetletop
-local s,id=GetID()
-function s.initial_effect(c)
-	--pendulum summon
-	Pendulum.AddProcedure(c)
-end
